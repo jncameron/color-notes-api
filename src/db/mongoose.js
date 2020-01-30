0e1b9e229ab53d86996cc1892b29a807e26ccdf3
John Cameron <john.n.cameron@gmail.com>
2020-01-30 18:51:46 +1100

    env variables

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  "mongodb+srv://jncamdev:CAMERONDEV@cluster0-i65n7.mongodb.net/test?retryWrites=true",
+  `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0-i65n7.mongodb.net/${process.env.MONGODB_NAME}?retryWrites=true`,
   {
     useNewUrlParser: true,
     useCreateIndex: true
